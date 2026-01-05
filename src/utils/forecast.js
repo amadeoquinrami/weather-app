7135ac46811c71209441da0436cbdd30a1f62ab7
Amadeo Ramirez <amadeoramirez@MacBookPro.lan>
2026-01-05 13:51:57 -0500

    adjusting files

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,16 @@ request({ url , json: true}, (error, {body}) => {
     } else if ( body.error) {
             callback('Unable to find location', undefined)
     } else {
-            callback(undefined, `${ body.current.weather_descriptions[0]} . It is currently ${ body.current.temperature} degress out. It feels like ${ body.current.feelslike} degress.`)
+
+                const forecast = `${body.current.weather_descriptions[0]}.
+                It is Currently ${body.current.temperature} degrees out.
+                It feels like ${body.current.feelslike} degrees.
+                The Humidity is ${body.current.humidity}%.`
+
+                callback(undefined, forecast)
+
+        //     console.log(body.daily.data[0])    
+        //     callback(undefined, `${ body.current.weather_descriptions[0]} . It is currently ${ body.current.temperature} degress out. It feels like ${ body.current.feelslike} degress.`)
     }
 })
 }
